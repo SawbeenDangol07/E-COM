0e4a951eb169935251b8197ea45924755e15d199
SawbeenDangol07 <sabindangol1010@gmail.com>
2026-08-13 09:53:46 +0545

    cloudinary Service added

---
+++ src/services/cloudinary.service.js
@@ -0,0 +1,34 @@
+const { CloudinaryConfig } = require("../config/app.config");
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
+
+class CloudinaryService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: CloudinaryConfig.className,
+      api_key: CloudinaryConfig.apiKey,
+      api_secret: cloudinary.api_secret,
+    });
+  }
+
+  async singlefileUpload(filepath, dir = "/") {
+    try {
+      const response = await cloudinary.uploader.upload(filepath, {
+        folder: "/api-55" + dir,
+      });
+      fs.unlinkSync(filepath);
+      return {
+        public_id: response.public_id,
+        url: response.secure_url,
+      };
+    } catch (exception) {
+      throw {
+        code: 500,
+        message: "Cloudinary file upload failed",
+        status: "CLOUDINARY_FILE_UPLOAD_ERR",
+      };
+    }
+  }
+}
+
+module.exports = new CloudinaryService();
