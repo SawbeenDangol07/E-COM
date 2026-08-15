992eb19e9a2049f97488def974ff869f70447b82
SawbeenDangol07 <sabindangol1010@gmail.com>
2026-08-15 08:28:54 +0545

    auth router updated

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,16 +1,24 @@
+const checkLogin = require("../../middlewares/auth.middleware");
 const uploader = require("../../middlewares/uploader.middleware");
+const bodyValidator = require("../../middlewares/validation.middleware");
 const authCtrl = require("./auth.controller");
+const { RegisterDTO, LoginDTO } = require("./auth.validator");
 
 const authRouter = require("express").Router();
 
 authRouter.post(
   "/register",
   uploader().single("image"),
+  bodyValidator(RegisterDTO),
   authCtrl.registerFunction,
 );
+
 authRouter.get("/activate/:token", authCtrl.activateUser);
+
 authRouter.get("/reactivate/:token", authCtrl.reactivateUser);
-authRouter.post("/login", authCtrl.loginFunction);
+
+authRouter.post("/login", bodyValidator(LoginDTO), authCtrl.loginFunction);
+
 authRouter.get("/me", checkLogin(), authCtrl.getLoggedInUser);
 
 module.exports = authRouter;
