4c1e1dd3080a72eea778e6050ae64927e80a1643
SawbeenDangol07 <sabindangol1010@gmail.com>
2026-08-15 07:33:34 +0545

    registration added

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,8 +1,10 @@
 const { AppConfig } = require("../../config/app.config");
+const { Status } = require("../../config/constant");
 const userService = require("../user/user.service");
 const authService = require("./auth.service");
 
 class AuthController {
+  //register function for registering user
   async registerFunction(req, res, next) {
     try {
       const data = authService.transformForUser(req);
@@ -25,24 +27,65 @@ class AuthController {
       next(exception);
     }
   }
-  async loginFunction(req, res, next) {
-    try {
-    } catch (exception) {
-      next(exception);
-    }
-  }
+
+  //activating registered user
   async activateUser(req, res, next) {
     try {
+      const token = req.params.token;
+      const user = await userService.getSingleUserByFilter({ token: token });
+
+      if (!user) {
+        throw {
+          code: 422,
+          message: "Token not found",
+          status: "TOKEN_NOT_FOUND",
+        };
+      }
+
+      const today = Date.now();
+      const expiryTime = user.expiryTime;
+
+      if (today > expiryTime) {
+        throw {
+          code: 422,
+          message: "Activation token expired",
+          status: "ACTIVATION_TOKEN_EXPIRED",
+        };
+      }
+
+      user.status = Status.ACTIVE;
+      user.token = null;
+      user.expiryTime = null;
+
+      await user.save();
+
+      res.json({
+        data: userService.getPublicProfileOfUser(user),
+        message: "User activated successfully",
+        status: "OK",
+      });
     } catch (exception) {
       next(exception);
     }
   }
+
+  //reactivating user
   async reactivateUser(req, res, next) {
     try {
     } catch (exception) {
       next(exception);
     }
   }
+
+  //login function for the registered user
+  async loginFunction(req, res, next) {
+    try {
+    } catch (exception) {
+      next(exception);
+    }
+  }
+
+  //get function for logged in user
   async getLoggedInUser(req, res, next) {
     try {
     } catch (exception) {
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -9,6 +9,28 @@ class userService {
       throw exception;
     }
   }
+
+  async getSingleUserByFilter(filter) {
+    try {
+      const user = await UserModel.findOne(filter);
+      return user;
+    } catch (exception) {
+      throw exception;
+    }
+  }
+
+  getPublicProfileOfUser(user) {
+    const userObj = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      image: user.image,
+      status: user.status,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
 }
 
 module.exports = new userService();
